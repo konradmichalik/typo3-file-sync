5dc5754feb27be3846325b22a66f0ff1b7911a39
Konrad Michalik <km@move-elevator.de>
2026-03-03 14:50:47 +0100

    chore: add release and maintenance tooling

---
+++ packaging_exclude.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the "typo3_file_sync" TYPO3 CMS extension.
+ *
+ * (c) 2025 Konrad Michalik <hej@konradmichalik.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    'directories' => [
+        '.build',
+        '.ddev',
+        '.git',
+        '.github',
+        'bin',
+        'build',
+        'public',
+        'resources\\/private\\/frontend',
+        'resources\\/private\\/libs\\/build',
+        'tailor-version-upload',
+        'tests',
+        'var',
+        'vendor',
+    ],
+    'files' => [
+        'DS_Store',
+        'CODE_OF_CONDUCT.md',
+        'codeception.yml',
+        'codecov.yml',
+        'CODEOWNERS',
+        'composer.lock',
+        'composer-require-checker.json',
+        'CONTRIBUTING.md',
+        'crowdin.yaml',
+        'dependency-checker.json',
+        'docker-compose.yml',
+        'editorconfig',
+        'editorconfig-lint.php',
+        'gitattributes',
+        'gitignore',
+        'packaging_exclude.php',
+        'php-cs-fixer.php',
+        'phpstan.php',
+        'phpstan-baseline.neon',
+        'phpunit.functional.xml',
+        'phpunit.unit.xml',
+        'phpunit.xml',
+        'phpstan.neon',
+        'rector.php',
+        'renovate.json',
+        'typoscript-lint.yml',
+        'version-bumper.yaml',
+    ],
+];
