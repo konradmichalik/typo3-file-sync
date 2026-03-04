b3d8d25d2a1f108191abaafa8dfa0c829aee71b5
Konrad Michalik <km@move-elevator.de>
2026-03-04 10:18:15 +0100

    chore: enable debug logging for file sync operations in sitepackage

--- Tests/Acceptance/Fixtures/packages/sitepackage/ext_localconf.php
+++ Tests/Acceptance/Fixtures/packages/sitepackage/ext_localconf.php
@@ -2,8 +2,8 @@
 
 declare(strict_types=1);
 
-use KonradMichalik\Typo3FileSync\Resource\Handler\PlaceholderImageResource;
-use KonradMichalik\Typo3FileSync\Resource\Handler\RemoteInstanceResource;
+use Psr\Log\LogLevel;
+use TYPO3\CMS\Core\Log\Writer\FileWriter;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 defined('TYPO3') or die();
@@ -25,5 +25,14 @@ $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['typo3_file_sync']['storages'][1] = [
     [
         'identifier' => 'placeholder_image',
         'configuration' => '#CCCCCC, #969696',
+    ],
+];
+
+// Enable debug logging for file sync operations
+$GLOBALS['TYPO3_CONF_VARS']['LOG']['KonradMichalik']['Typo3FileSync']['writerConfiguration'] = [
+    LogLevel::DEBUG => [
+        FileWriter::class => [
+            'logFile' => \TYPO3\CMS\Core\Core\Environment::getVarPath() . '/log/typo3_file_sync.log',
+        ],
     ],
 ];
