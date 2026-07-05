894c25e0ccbbae7e2fba27ec293b279d28a797df
Konrad Michalik <konrad.michalik@gmx.de>
2026-07-05 11:28:22 +0200

    perf: add index on sys_file sync identifier column

--- ext_tables.sql
+++ ext_tables.sql
@@ -3,7 +3,9 @@
 #
 CREATE TABLE sys_file (
     tx_typo3_file_sync_identifier varchar(255) DEFAULT '' NOT NULL,
-    tx_typo3_file_sync_tstamp int(11) DEFAULT '0' NOT NULL
+    tx_typo3_file_sync_tstamp int(11) DEFAULT '0' NOT NULL,
+
+    KEY tx_typo3_file_sync_identifier (tx_typo3_file_sync_identifier)
 );
 
 #
